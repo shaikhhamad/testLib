caf5c7279ce0d0f31baa87ef63ac87df447c266b
shaikhhamad <shaikhhamadali@gmail.com>
2017-08-01 19:38:32 +0500

    added helper method

---
+++ Test/mylibrary/src/main/java/com/hamadshaikh/mylibrary/Helper.java
@@ -0,0 +1,11 @@
+package com.hamadshaikh.mylibrary;
+
+/**
+ * Created by usmankhalil on 01/08/2017.
+ */
+
+public class Helper {
+    public void print(String name){
+        System.out.print("Welcome to your lib Mr. "+name);
+    }
+}
