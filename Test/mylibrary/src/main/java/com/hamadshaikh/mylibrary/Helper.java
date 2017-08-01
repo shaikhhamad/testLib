cc8f1612866714e4a336cffa4bb75fbfdfc22f8f
shaikhhamad <shaikhhamadali@gmail.com>
2017-08-01 19:47:10 +0500

    2nd commit

--- Test/mylibrary/src/main/java/com/hamadshaikh/mylibrary/Helper.java
+++ Test/mylibrary/src/main/java/com/hamadshaikh/mylibrary/Helper.java
@@ -1,5 +1,7 @@
 package com.hamadshaikh.mylibrary;
 
+import android.util.Log;
+
 /**
  * Created by usmankhalil on 01/08/2017.
  */
@@ -7,5 +9,6 @@ package com.hamadshaikh.mylibrary;
 public class Helper {
     public void print(String name){
         System.out.print("Welcome to your lib Mr. "+name);
+        Log.i("Test","Welcome to your lib Mr. "+name);
     }
 }
